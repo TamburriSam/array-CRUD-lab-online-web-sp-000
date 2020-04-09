08db16f43a8a2ad602b37d424ccc61ccf5861c60
Sam Tamburri <tamburrisam@gmail.com>
2020-04-09 19:30:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,39 @@
+supplements = ["turmeric", "curcumin", "ginger", "vitamin d3"]
+element =
 def create_an_empty_array
-  
+  supplements = []
 end
 
 def create_an_array
-  
+  supplements = ["turmeric", "curcumin", "ginger", "vitamin d3"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+
+  supplements
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+supplements = ["turmeric", "curcumin", "ginger", "vitamin d3"]
+turmeric = supplements.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
